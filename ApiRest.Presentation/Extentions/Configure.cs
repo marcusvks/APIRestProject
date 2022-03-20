20e038bf146c91eee2fd47f8e6abf0b6f48fc566
Marcus Vinicius Kayser Smiderle <marcus.kayser@hotmail.com>
2022-03-20 00:47:23 -0300

    parametros banco docker

--- ApiRest.Presentation/Extentions/Configure.cs
+++ ApiRest.Presentation/Extentions/Configure.cs
@@ -10,11 +10,19 @@ namespace ApiRest.Presentation.Extentions
     {
         public static void ConfigureConnectionString(WebApplicationBuilder builder)
         {
+            string passDecrypted = default;
+
+            string passNormal = builder.Configuration.GetSection("ConnectionString")["PasswordNormal"];
+
             string passEncrypted = builder.Configuration.GetSection("ConnectionString")["PasswordEncrypted"];
+            
             var user = builder.Configuration.GetSection("ConnectionString")["User"];
             var server = builder.Configuration.GetSection("ConnectionString")["Server"];
 
-            string passDecrypted = Decrypter.Decrypter.Decrypt(passEncrypted);
+            if (string.IsNullOrEmpty(passNormal))
+                passDecrypted = Decrypter.Decrypter.Decrypt(passEncrypted);
+            else
+                passDecrypted = passNormal;
 
             var ConnectionString = $"user id={user};password={passDecrypted};server={server};database=ApiRestDatabase;connection timeout=150;MultipleActiveResultSets=true";
 
